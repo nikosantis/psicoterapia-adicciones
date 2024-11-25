f86c827716639c9c54d2bec0571f19aabfde8114
nikosantis <nikosantis@gmail.com>
2024-11-25 15:27:24 -0300

    feat: update form 2

--- src/components/wsp-content.tsx
+++ src/components/wsp-content.tsx
@@ -2,7 +2,6 @@ import Email from './form/email'
 import Name from './form/name'
 import Phone from './form/phone'
 import Profession from './form/profession'
-import SubmitButton from './form/submit-button'
 import WspFormSubmit from './form/wsp-form-submit'
 
 export default function WspContent() {
@@ -22,10 +21,6 @@ export default function WspContent() {
 
       <div className='block w-full'>
         <Phone />
-      </div>
-
-      <div className='w-full'>
-        <SubmitButton />
       </div>
     </WspFormSubmit>
   )
